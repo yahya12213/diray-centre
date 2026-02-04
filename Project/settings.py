848b05245b8ba206e77da0acd167d04513c93603
yahya12213 <yazmmat@gmail.com>
2026-02-04 17:50:48 +0100

    Update settings for production environment
    Set DEBUG to False and specify allowed hosts for production.

--- Project/settings.py
+++ Project/settings.py
@@ -31,9 +31,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qhs7@p$d)x%ztw(9d!rd0sv-35*3#blk+6*vtp)vzatrps0kaj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+CSRF_TRUSTED_ORIGINS = [
+    "https://web-production-7b55d.up.railway.app",
+]
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "web-production-7b55d.up.railway.app",
+    "localhost",
+    "127.0.0.1",
+]
+
 
 
 # Application definition
